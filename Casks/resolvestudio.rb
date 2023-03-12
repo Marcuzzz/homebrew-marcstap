db6802971f08f3c7d2b4e936e99967d3c397cda2
Marc <marc@mustbe.nl>
2023-03-12 15:35:11 +0100

    up

--- Casks/resolvestudio.rb
+++ Casks/resolvestudio.rb
@@ -2,7 +2,7 @@ cask 'resolvestudio' do
   version '1'
   sha256 :no_check
 
-  url "file://#{Dir.home}/pCloud\ Drive/00\ Marc/40\ Apps/mac/DaVinci_Resolve_Studio_18.1.4_Mac.zip"
+  url "file://#{Dir.home}/pCloud%20Drive/00%20Marc/40%20Apps/mac/DaVinci_Resolve_Studio_18.1.4_Mac.zip"
   name 'resolvestudio'
   homepage 'https://www.blackmagicdesign.com/products/davinciresolve'
   
