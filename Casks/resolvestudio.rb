889d1bbc234fe23b7d89d83177f8a49525ec5a8b
Marc <marc@mustbe.nl>
2023-03-12 15:41:46 +0100

    up

--- Casks/resolvestudio.rb
+++ Casks/resolvestudio.rb
@@ -1,11 +1,12 @@
 cask 'resolvestudio' do
-  version '1'
+  version '18.1.4'
   sha256 :no_check
 
-  url "file://#{Dir.home}/pCloud%20Drive/00%20Marc/40%20Apps/mac/DaVinci_Resolve_Studio_18.1.4_Mac.zip"
+  url "file://#{Dir.home}/pCloud%20Drive/00%20Marc/40%20Apps/mac/DaVinci_Resolve_Studio_#{version}_Mac.zip"
   name 'resolvestudio'
   homepage 'https://www.blackmagicdesign.com/products/davinciresolve'
   
   #app "Visual Studio Code.app", target: "Visual Studio Code Intel.app"
+  pkg "Install Resolve #{version}.pkg"
   
 end
