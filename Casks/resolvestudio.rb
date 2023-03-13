0c068744d374d64189d4ebea96e5696d192b5180
Marc <marc@mustbe.nl>
2023-03-13 08:33:15 +0100

    up

--- Casks/resolvestudio.rb
+++ Casks/resolvestudio.rb
@@ -1,19 +1,19 @@
-cask 'resolvestudio' do
-  version '18.1.4'
-  major_version = version.split('.').first
+cask "resolvestudio" do
+  version "18.1.4"
+  major_version = version.split(".").first
   sha256 :no_check
 
   url "file://#{Dir.home}/pCloud%20Drive/00%20Marc/40%20Apps/mac/DaVinci_Resolve_Studio_#{version}_Mac.zip"
-  name 'resolvestudio'
-  homepage 'https://www.blackmagicdesign.com/products/davinciresolve'
+  name "resolvestudio"
+  homepage "https://www.blackmagicdesign.com/products/davinciresolve"
   
   #app "Visual Studio Code.app", target: "Visual Studio Code Intel.app"
   pkg "Install Resolve #{version}.pkg"
   
   postflight do
   	# create symlink using ln -s
-  	system_command '/bin/rm', args: ['-rf','/Applications/DaVinci Resolve #{major_version}']
-  	system_command '/bin/mv', args: ['/Applications/DaVinci Resolve', '/Applications/DaVinci Resolve #{major_version} Studio']
+  	system_command "/bin/rm", args: ["-rf","/Applications/DaVinci Resolve #{major_version} Studio"]
+  	system_command "/bin/mv", args: ["/Applications/DaVinci Resolve", "/Applications/DaVinci Resolve #{major_version} Studio"]
   end
   
 end
