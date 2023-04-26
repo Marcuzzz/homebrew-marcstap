09bc97246999df0dae30e1888953190fd8e8eccd
Marcuzzz <marc@mustbe.nl>
2023-04-26 19:40:31 +0200

    Update zxpsigncmd.rb

--- Casks/zxpsigncmd.rb
+++ Casks/zxpsigncmd.rb
@@ -16,7 +16,8 @@ cask 'zxpsigncmd' do
   
   postflight do
   	# create symlink using ln -s
-  	system_command '/bin/ln', args: ['-s', '/opt/homebrew/Caskroom/zxpsigncmd/1/ZXPSignCmd/ZXPSignCmd-64bit', '/usr/local/bin/ZXPSignCmd']
+  	system_command '/bin/ln', args: ['-s', '/opt/homebrew/Caskroom/zxpsigncmd/1/ZXPSignCmd/ZXPSignCmd-64bit', '/usr/local/bin/ZXPSignCmd'] unless !Dir.exist?("/opt/homebrew/Caskroom/zxpsigncmd/1/ZXPSignCmd/ZXPSignCmd-64bit")
+    system_command '/bin/ln', args: ['-s', '/usr/local/Caskroom/zxpsigncmd/1/ZXPSignCmd/ZXPSignCmd-64bit', '/usr/local/bin/ZXPSignCmd'] unless !Dir.exist?("/usr/local/Caskroom/zxpsigncmd/1/ZXPSignCmd/ZXPSignCmd-64bit")
   end
 
 end
