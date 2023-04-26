801d20b3ca63431ade6ccaea1e614323c6097808
Marcuzzz <marc@mustbe.nl>
2023-04-26 19:39:00 +0200

    Update s3-sftp-user.rb

--- Casks/s3-sftp-user.rb
+++ Casks/s3-sftp-user.rb
@@ -19,7 +19,7 @@ cask 's3-sftp-user' do
   	system_command '/bin/mkdir', args: ["#{Dir.home}/MediaMonks"] unless Dir.exist?("#{Dir.home}/MediaMonks")
   	system_command '/bin/rm', args: ['-rf',"#{Dir.home}/MediaMonks/s3-sftp-user"] unless !Dir.exist?("#{Dir.home}/MediaMonks/s3-sftp-user")
   	system_command '/bin/mv', args: ['/opt/homebrew/Caskroom/s3-sftp-user/1/s3-sftp-user', "#{Dir.home}/MediaMonks/s3-sftp-user"] unless !Dir.exist?("/opt/homebrew/Caskroom/s3-sftp-user/1/s3-sftp-user")
-    system_command '/bin/mv', args: ['/usr/local/Caskroom/s3-sftp-user/1/s3-sftp-user', "#{Dir.home}/MediaMonks/s3-sftp-user"] unless !Dir.exist?("/usr/local/Caskroom/s3-sftp-user/1/s3-sftp-user")
+  	system_command '/bin/mv', args: ['/usr/local/Caskroom/s3-sftp-user/1/s3-sftp-user', "#{Dir.home}/MediaMonks/s3-sftp-user"] unless !Dir.exist?("/usr/local/Caskroom/s3-sftp-user/1/s3-sftp-user")
   end
 
 end
