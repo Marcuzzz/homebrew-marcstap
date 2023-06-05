91b96b8eef825019a0148f3e3d994b8a0e79eb1d
Toni Magni <github@afm.co>
2023-06-05 18:36:26 +0200

    Fixed accidentally hard coded version in freefilesync.rb

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -2,7 +2,7 @@ cask 'freefilesync' do
   version '12.3'
   sha256 :no_check
 
-  url "https://github.com/hkneptune/FreeFileSync/releases/download/v12.3/FreeFileSync_#{version}_macOS.zip"
+  url "https://github.com/hkneptune/FreeFileSync/releases/download/v#{version}/FreeFileSync_#{version}_macOS.zip"
   name 'freefilesync'
   homepage 'https://github.com/hkneptune/FreeFileSync/releases'
 
